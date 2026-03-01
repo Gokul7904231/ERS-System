2a2d9bbb2c7490a18266ca71254749d600206512
Gokul7904231 <asgokul2004@gmail.com>
2026-03-01 13:08:33 +0530

    Fix emotion package structure

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import sys
 import os
 
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 ROOT = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(ROOT)
 sys.path.append(os.path.join(ROOT, "emotion"))
